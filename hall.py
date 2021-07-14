4787ed3c6b3d561243213799c55bd995ac5a0ca6
Berkanyigit123 <berkanyigit.b@gmail.com>
2021-07-14 22:34:14 +0300

    Speed-final

--- hall.py
+++ hall.py
@@ -1,32 +1,46 @@
-#!/usr/bin/env python
 
-import time
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+from time import sleep
+import time, math
 
-import pigpio
+dist_meas = 0.00
+km_per_hour = 0
+rpm = 0
+elapse = 0
+sensor = 12
+pulse = 0
+start_timer = time.time()
 
-#
-# OH3144E or equivalent Hall effect sensor
-#
-# Pin 1 - 5V
-# Pin 2 - Ground
-# Pin 3 - gpio (here P1-8, gpio 14, TXD is used)
-#
-# The internal gpio pull-up is enabled so that the sensor
-# normally reads high.  It reads low when a magnet is close.
-#
+def init_GPIO():					# initialize GPIO
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(sensor,GPIO.IN,GPIO.PUD_UP)
 
-HALL=14
+def calculate_elapse(channel):				# callback function
+	global pulse, start_timer, elapse
+	pulse+=1								# increase pulse by 1 whenever interrupt occurred
+	elapse = time.time() - start_timer		# elapse for every 1 complete rotation made!
+	start_timer = time.time()				# let current time equals to start_timer
 
-pi = pigpio.pi() # connect to local Pi
+def calculate_speed(r_cm):
+	global pulse,elapse,rpm,dist_km,dist_meas,km_per_sec,km_per_hour
+	if elapse !=0:							# to avoid DivisionByZero error
+		rpm = 1/elapse * 60
+		circ_cm = (2*math.pi)*r_cm			# calculate wheel circumference in CM
+		dist_km = circ_cm/100000 			# convert cm to km
+		km_per_sec = dist_km / elapse		# calculate KM/sec
+		km_per_hour = km_per_sec * 3600		# calculate KM/h
+		dist_meas = (dist_km*pulse)*1000	# measure distance traverse in meter
+		return km_per_hour
 
-pi.set_mode(HALL, pigpio.INPUT)
-pi.set_pull_up_down(HALL, pigpio.PUD_UP)
+def init_interrupt():
+	GPIO.add_event_detect(sensor, GPIO.FALLING, callback = calculate_elapse, bouncetime = 20)
 
-start = time.time()
-
-while (time.time() - start) < 60:
-   print("Hall = {}".format(pi.read(HALL)))
-   time.sleep(0.2)
-
-pi.stop()
-
+if __name__ == '__main__':
+	init_GPIO()
+	init_interrupt()
+	while True:
+		calculate_speed(20)	# call this function with wheel radius as parameter
+  		print('rpm:{0:.0f}-RPM kmh:{1:.0f}-KMH dist_meas:{2:.2f}m pulse:{3}'.format(rpm,km_per_hour,dist_meas,pulse))
+		sleep(0.30)
